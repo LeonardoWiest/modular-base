b4ac846140d15a076ee7ec644353f04e0f0aa0e9
Leonardo Wiest <leonardowiest@hotmail.com>
2019-07-12 21:46:17 -0300

    Adicionado Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,7 +11,9 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
