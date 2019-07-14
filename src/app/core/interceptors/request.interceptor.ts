c49e91e924575fd5264ce13e6976ae910bd2c12d
Leonardo Wiest <leonardowiest@hotmail.com>
2019-07-14 01:11:31 -0300

    Iniciado interceptador de request

---
+++ src/app/core/interceptors/request.interceptor.ts
@@ -0,0 +1,38 @@
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse
+} from '@angular/common/http';
+
+export abstract class RequestInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const cloneRequest = request.clone();
+
+    return next.handle(request).pipe(
+      tap((event: HttpEvent<any>) =>
+        event instanceof HttpResponse
+          ? () => this.processarResponseSucesso(event)
+          : event
+      ),
+      catchError((error: HttpErrorResponse) =>
+        this.processarResponseFalha(error, cloneRequest)
+      )
+    );
+  }
+  private processarResponseSucesso(response: HttpResponse<any>) {}
+
+  private processarResponseFalha(
+    response: HttpErrorResponse,
+    request: HttpRequest<any>
+  ) {
+    return throwError(response.error);
+  }
+}
--- src/app/shared/helpers/http.request.service.ts
+++ src/app/shared/helpers/http.request.service.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 /**
@@ -7,6 +8,12 @@ import { Injectable } from '@angular/core';
  * Utiliário para execução de requisições HTTP.
  */
 @Injectable()
-export abstract class HttpRequestServiceHelper {
-  constructor(private http: HttpClient) {}
+export abstract class HttpRequestServiceHelper extends HttpClient {
+  constructor(private _handler: HttpHandler) {
+    super(_handler);
+  }
+
+  post<T>(): Observable<T> {
+    return null;
+  }
 }
