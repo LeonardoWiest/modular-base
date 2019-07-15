1105f6988a47b293d637386c71be626e0f4704af
Leonardo Wiest <leonardowiest@hotmail.com>
2019-07-15 19:29:36 -0300

    Configuração integração continuo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { BotaoPrimarioComponent } from '@shared/components/botao-primario/botao-primario.component';
-import { InputPrimarioComponent } from '@shared/components/input-primario/input-primario.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, RouterModule],
-  declarations: [InputPrimarioComponent, BotaoPrimarioComponent],
-  exports: [
-    CommonModule,
-    FormsModule,
-    InputPrimarioComponent,
-    BotaoPrimarioComponent
-  ]
+  declarations: [],
+  exports: [CommonModule, FormsModule]
 })
 export class SharedModule {}
